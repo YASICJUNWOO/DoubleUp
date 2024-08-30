321aca559d9f902c126744e15ac5c86396872660
planit-mason <mason@planitsquare.com>
2024-08-30 20:05:07 +0900

    init

---
+++ src/main/java/com/junwoo/doubleup/DoubleUpApplication.java
@@ -0,0 +1,13 @@
+package com.junwoo.doubleup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleUpApplication.class, args);
+	}
+
+}
