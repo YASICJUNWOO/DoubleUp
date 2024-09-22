fe6c27963a3866789235c3010bca271d75a7bb8e
YASICJUNWOO <joonoo3@naver.com>
2024-09-22 21:03:52 +0900

    feat : 거래소, 유형 필터 적용
    #10

--- webapp/src/components/stock/StockList.tsx
+++ webapp/src/components/stock/StockList.tsx
@@ -3,46 +3,72 @@ import {Table, TableProps} from "antd";
 import {IStock} from "../../interface/interface";
 import axios from "axios";
 
+type DataIndex = keyof IStock;
+
 export const StockList: React.FC = () => {
 
     const [stocks, setStocks] = useState<IStock[]>([]);
 
     useEffect(() => {
         axios.get('/api/stock/all')
-        	.then(response => {
-				setStocks(response.data);
-			})
-			.catch(error => {
-				console.error(error);
-			});
+            .then(response => {
+                setStocks(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }, []);
 
     const columns: TableProps<IStock>['columns'] = [
         {
             title: '티커',
-			dataIndex: 'symbol',
-			key: 'symbol',
-		},
-		{
-			title: '이름',
-			dataIndex: 'name',
-			key: 'name',
-		},
-		{
-			title: '거래소',
-			dataIndex: 'market',
-			key: 'market',
-		},
-		{
-			title: '유형',
-			dataIndex: 'stockType',
-			key: 'stockType',
+            dataIndex: 'symbol',
+            key: 'symbol',
+        },
+        {
+            title: '이름',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: '거래소',
+            dataIndex: 'market',
+            key: 'market',
+            filters: [
+                {
+                    text: '한국',
+                    value: 'korea',
+                    children: [
+                        {text: '코스피', value: 'KOSPI'},
+                        {text: '코스닥', value: 'KOSDAQ'}
+                    ]
+                },
+                {
+                    text: '미국',
+                    value: 'usa',
+                    children: [
+                        {text: 'NYSE', value: 'NYSE'},
+                        {text: 'NASDAQ', value: 'NASDAQ'}
+                    ]
+                }
+            ],
+            onFilter: (value, record) => record.market.indexOf(value as string) === 0
+        },
+        {
+            title: '유형',
+            dataIndex: 'stockType',
+            key: 'stockType',
+            filters: [
+                {text: '주식', value: 'COMMON'},
+                {text: 'ETF', value: 'ETF'}
+            ],
+            onFilter: (value, record) => record.stockType.indexOf(value as string) === 0
         }
     ];
 
     return (
         <div>
-			<Table dataSource={stocks} columns={columns} rowKey="id"/>
-		</div>
-	)
+            <Table dataSource={stocks} columns={columns} rowKey="id"/>
+        </div>
+    )
 }
